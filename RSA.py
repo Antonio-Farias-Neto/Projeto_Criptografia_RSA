1628f7b4b9e8955834833cf05d30f1f593d0804c
Antonio-Farias-Neto <antoniofarias1880@gmail.com>
2025-09-15 13:58:14 -0300

    Divisaõ inicial das partes do código

---
+++ RSA.py
@@ -0,0 +1,17 @@
+#Opção[A - Criptografar mensagem/ B - Descriptografar mensagem]
+
+#conversão de letra para número[A]
+
+#Blocagem[A]
+
+#Criptografar[A]
+
+
+#Blocagem[B]
+
+#Descriptografar[B]
+
+#Desblocar[B]
+
+#Conversão de número para Letra[B]
+
