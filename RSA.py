8a87d11cc1cad1e71019e0b4651b623062664ba4
Antonio Farias Lopes Neto <antoniofarias1880@gmail.com>
2025-09-19 09:10:39 -0300

    Realização da blocagem e da criptografia dos blocos

--- RSA.py
+++ RSA.py
@@ -12,27 +12,51 @@ def eh_primo(num):
 #Opção[A - Criptografar mensagem/ B - Descriptografar mensagem]
 
 #conversão de letra para número[A]
+mensagem = input("Mensagem: ")
+novamsg = ""
+
+for crp in mensagem:
+    novamsg += str(ord(crp))
 
 #Blocagem[A]
 
 p = 0
 q = 0
 while True:
-    p = random.randint(1000, 1000000)
+    p = random.randint(1000, 100000)
     if eh_primo(p):
         break
 
 while True:
-    q = random.randint(1000, 1000000)
+    q = random.randint(1000, 100000)
     if eh_primo(q) and q != p:
         break
-    
+        
 n = p*q
-y = 3
-chavePublica = (n,y)
+y = 3       #chavePublica = (n,y)
+
+blocosASeremCriptografados = []
+i = 0
+while i < len(novamsg):
+    for j in range(len(novamsg), i, -1):
+        bloco = novamsg[i:j]
+        if int(bloco) < n:
+            blocosASeremCriptografados.append(bloco)
+            i = j
+            break
+    else:
+        blocosASeremCriptografados.append(novamsg[i])
+        i += 1
+print(n)          
+print(novamsg)
+print(blocosASeremCriptografados)
 
 #Criptografar[A]
+blocosCriptografados = []
+for bloco in blocosASeremCriptografados:
+    blocosCriptografados.append((int(bloco)**y) % n)
 
+print(blocosCriptografados)
 #Blocagem[B]
 
 #Descriptografar[B]
@@ -40,4 +64,3 @@ chavePublica = (n,y)
 #Desblocar[B]
 
 #Conversão de número para Letra[B]
-
