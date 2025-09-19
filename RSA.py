c5101adb12d53d8e82bb24d2a8242625fa58db9b
Carlos Leal <carlsnascimento72@gmail.com>
2025-09-19 08:24:43 -0300

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,11 +1,45 @@
+import random
+
+def eh_primo(num):
+    if num % 2 == 0:
+        return False
+    for i in range(3, num, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
 #Opção[A - Criptografar mensagem/ B - Descriptografar mensagem]
+
+mensagem = input("Mensagem: ")
+novamsg = ""
+
+for crp in mensagem:
+    novamsg += (ord(crp))
+
 
 #conversão de letra para número[A]
 
 #Blocagem[A]
 
-#Criptografar[A]
+p = 0
+q = 0
+while True:
+    p = random.randint(1000, 1000000)
+    if eh_primo(p):
+        break
 
+while True:
+    q = random.randint(1000, 1000000)
+    if eh_primo(q) and q != p:
+        break
+
+n = p*q
+y = 3
+chavePublica = (n,y)
+
+
+#Criptografar[A]
 
 #Blocagem[B]
 
@@ -14,4 +48,5 @@
 #Desblocar[B]
 
 #Conversão de número para Letra[B]
+
 
