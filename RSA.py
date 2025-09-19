6d065e8bc043b7e273ea462e295d365109e0dfa9
Antonio Farias Lopes Neto <antoniofarias1880@gmail.com>
2025-09-18 23:59:36 -0300

    Criação do cálculo do "n" da chave pública

--- RSA.py
+++ RSA.py
@@ -1,11 +1,37 @@
+import random
+
+def eh_primo(num):
+    if num % 2 == 0:
+        return False
+    for i in range(3, num, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
 #Opção[A - Criptografar mensagem/ B - Descriptografar mensagem]
 
 #conversão de letra para número[A]
 
 #Blocagem[A]
 
-#Criptografar[A]
+p = 0
+q = 0
+while True:
+    p = random.randint(1000, 1000000)
+    if eh_primo(p):
+        break
 
+while True:
+    q = random.randint(1000, 1000000)
+    if eh_primo(q) and q != p:
+        break
+    
+n = p*q
+y = 3
+chavePublica = (n,y)
+
+#Criptografar[A]
 
 #Blocagem[B]
 
