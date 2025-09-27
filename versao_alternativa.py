ec8ed7cb1dec4111bb7620b1ddbeba536663e918
Davi de Lucena Vasconcelos <davilucenana@gmail.com>
2025-09-26 23:09:50 -0300

    versão funcionando(corrigida) + versao alternativa

--- versao_alternativa.py
+++ versao_alternativa.py
@@ -12,19 +12,19 @@ def eh_primo(num):
             return False
     return True
 
-
 #Opção[A - Criptografar mensagem/ B - Descriptografar mensagem]
 
 #conversão de letra para número[A]
 mensagem = input("Mensagem: ")
-novamsg = ""
 
+# Em vez de uma string gigante, vamos processar caractere por caractere
+caracteres_para_criptografar = []
 for crp in mensagem:
-    novamsg += str(ord(crp))
+    caracteres_para_criptografar.append(ord(crp))
 
+print("códigos dos caracteres:", caracteres_para_criptografar)
 
 #Blocagem[A]
-
 p = 0
 q = 0
 while True:
@@ -45,62 +45,48 @@ for candidato in range(3, 10):
     if eh_coprimo(candidato, m):
         y = candidato
         break
-    else:
-        for candidato in range(3, m, 2):  # Testa todos os ímpares a partir de 3
-            if eh_coprimo(candidato, m):
-                y = candidato
-                break
-
+else:
+    for candidato in range(3, m, 2):  # Testa todos os ímpares a partir de 3
+        if eh_coprimo(candidato, m):
+            y = candidato
+            break
 
 print("m: "+ str(m), "\ny:"+ str(y))
+print("n:" + str(n))
 #chavePublica = (n,y)
 
-blocosASeremCriptografados = []
-i = 0
-while i < len(novamsg):
-    for j in range(len(novamsg), i, -1):
-        bloco = novamsg[i:j]
-        if int(bloco) < n:
-            blocosASeremCriptografados.append(bloco)
-            i = j
-            break
-    else:
-        blocosASeremCriptografados.append(novamsg[i])
-        i += 1
-print("n:" + str(n))          
-print("mensagem: " + novamsg)
-print("em blocos: " + str(blocosASeremCriptografados))
+# Agora cada "bloco" é simplesmente um código de caractere
+blocosASeremCriptografados = caracteres_para_criptografar
+print("caracteres como números: " + str(blocosASeremCriptografados))
 
 #Criptografar[A]
 blocosCriptografados = []
 for bloco in blocosASeremCriptografados:
     blocosCriptografados.append((int(bloco)**y) % n)
 
-print("blocos criptografados: " + str(blocosCriptografados))
+print("códigos criptografados: " + str(blocosCriptografados))
 #Blocagem[B]
 
 #Descriptografar[B]
-
 d = pow(y, -1, m)  #chavePrivada = (n,d)
 print("d: "+ str(d))
 blocosDescriptografados = []
 for bloco in blocosCriptografados:
     blocosDescriptografados.append((int(bloco)**d) % n)
-print("blocos descriptografados: " + str(blocosDescriptografados))
+print("códigos descriptografados: " + str(blocosDescriptografados))
 
 #Conversão de número para Letra[B]
-msgDescriptografada = ""
-for bloco in blocosDescriptografados:
-    msgDescriptografada += str(bloco)
-    #msgDescriptografada = chr(msgDescriptografada)
-
-print("mensagem descriptografada: " + msgDescriptografada)
 msgfinal = ""
-for num in range(0, len(msgDescriptografada), 2):
-    num = msgDescriptografada[num:num+2]
-    msgfinal += chr(int(num))
+for codigo in blocosDescriptografados:
+    try:
+        msgfinal += chr(int(codigo))
+    except (ValueError, OverflowError):
+        msgfinal += "?"
+
 print("mensagem final: " + msgfinal)
 
-
-
-
+# Verificação
+print("\nVerificação:")
+print("Original:", mensagem)
+print("Final:   ", msgfinal)
+print("Iguais:  ", mensagem == msgfinal)
